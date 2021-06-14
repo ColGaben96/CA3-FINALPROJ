538750e0dbec3b7e094ab7cff8e87e5a86f1289b
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2021-06-13 19:47:39 -0500

    Optimized code | Gabriel Blanco

--- src/co/edu/unbosque/model/LogicaSolitario.java
+++ src/co/edu/unbosque/model/LogicaSolitario.java
@@ -3,95 +3,97 @@ package co.edu.unbosque.model;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * @author Sebastian Moncaleano
+ * @version 1.0
+ */
 public class LogicaSolitario {
-	
-	private Tablero tablero;
-	private List<int[][]> solucion;
-	private String[] direcciones;
-	
-	private int[][] matrizInicial =  {
-			{0, 0, 1, 1, 1, 0, 0},{0, 0, 1, 1, 1, 0, 0}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 2, 1, 1, 1}, 
-			{1, 1, 1, 1, 1, 1, 1}, {0, 0, 1, 1, 1, 0, 0}, {0, 0, 1, 1, 1, 0, 0}};
-	
-	public LogicaSolitario() {
-		tablero = new Tablero();
-		solucion = new ArrayList<int[][]>();
-		direcciones = tablero.getDirecciones();
-		solucion.add(matrizInicial);
-		
-		boolean solucionado = iterarMatrices(1);
-		if(solucionado) {
-			System.out.println("¡Se encontró la solución!");
-			imprimirSolucion();
-		}else {
-			System.out.println("****No hay Solución**********");
-			System.out.println("A continuación se ven las decisiones de la solución");
-		}
-	}
-	
-	public boolean iterarMatrices(int posicion) {
-		for(int i=0;i<tablero.getAncho();i++) {
-			for(int j=0;j<tablero.getAlto();j++) {
-				
-				for(int k = 0;k<direcciones.length;k++) {
-					
-					if(tablero.move(i, j, direcciones[k])) {
-						System.out.println("Se hace Movimiento desde la posición "+i+" - "+j);
-						imprimirMatriz(tablero.getMatriz());
-						if(solucion.size()<=posicion) {
-							int[][] matriz = new int[7][7];
-							solucion.add(matriz);
-						}
-						tablero.rellenarMatriz(tablero, solucion.get(posicion));
-							if(!tablero.isSolucion(tablero.getMatriz())) {
-							if(iterarMatrices(posicion +1)) {
-								return true;
-							}else {
-								tablero.retroceder(i, j,direcciones[k]);
-								System.out.println("No hay solución, Se hace backingtrack a la posición "+i+" - "+j);
-								imprimirMatriz(tablero.getMatriz());
-							}
-						} else {
-							return true;
-						}
-						
-						
-					}
-					
-				}
-			}
-			
-			
-		}
-		return false;
-	}
-	
-	
-	public void imprimirSolucion() {
-		
-		for(int i=0;i<solucion.size();i++) {
-			System.out.println("Paso Número: "+(i+1)+"\n");
-		for(int j=0;j<7;j++) {
-			for(int k=0;k<7;k++) {
-				System.out.print(solucion.get(i)[j][k]+" ");
-			}
-			System.out.println();
-		}
-		System.out.println();
-		}
-	}
-	
-	
-	public void imprimirMatriz(int[][] matriz) {
-		for(int i=0;i<matriz.length;i++) {
-			for(int j=0;j<matriz[0].length;j++) {
-				System.out.print(matriz[i][j]+" ");
-			}
-			System.out.println();
-		}
-		System.out.println();
-	}
-	
-	
+
+    private final Tablero tablero;
+    private final List<int[][]> solucion;
+    private final String[] direcciones;
+
+    public LogicaSolitario() {
+        tablero = new Tablero();
+        solucion = new ArrayList<>();
+        direcciones = tablero.getDirecciones();
+        int[][] matrizInicial = {
+                {0, 0, 1, 1, 1, 0, 0}, {0, 0, 1, 1, 1, 0, 0}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 2, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1}, {0, 0, 1, 1, 1, 0, 0}, {0, 0, 1, 1, 1, 0, 0}};
+        solucion.add(matrizInicial);
+
+        boolean solucionado = iterarMatrices(1);
+        if (solucionado) {
+            System.out.println("Â¡Se encontrÃ³ la soluciÃ³n!");
+            imprimirSolucion();
+        } else {
+            System.out.println("****No hay SoluciÃ³n**********");
+            System.out.println("A continuaciÃ³n se ven las decisiones de la soluciÃ³n");
+        }
+    }
+
+    public boolean iterarMatrices(int posicion) {
+        for (int i = 0; i < Tablero.getAncho(); i++) {
+            for (int j = 0; j < Tablero.getAlto(); j++) {
+
+                for (String direccione : direcciones) {
+
+                    if (tablero.move(i, j, direccione)) {
+                        System.out.println("Se hace Movimiento desde la posiciÃ³n " + i + " - " + j);
+                        imprimirMatriz(tablero.getMatriz());
+                        if (solucion.size() <= posicion) {
+                            int[][] matriz = new int[7][7];
+                            solucion.add(matriz);
+                        }
+                        tablero.rellenarMatriz(tablero, solucion.get(posicion));
+                        if (!tablero.isSolucion(tablero.getMatriz())) {
+                            if (iterarMatrices(posicion + 1)) {
+                                return true;
+                            } else {
+                                tablero.retroceder(i, j, direccione);
+                                System.out.println("No hay soluciÃ³n, Se hace backingtrack a la posiciÃ³n " + i + " - " + j);
+                                imprimirMatriz(tablero.getMatriz());
+                            }
+                        } else {
+                            return true;
+                        }
+
+
+                    }
+
+                }
+            }
+
+
+        }
+        return false;
+    }
+
+
+    public void imprimirSolucion() {
+
+        for (int i = 0; i < solucion.size(); i++) {
+            System.out.println("Paso NÃºmero: " + (i + 1) + "\n");
+            for (int j = 0; j < 7; j++) {
+                for (int k = 0; k < 7; k++) {
+                    System.out.print(solucion.get(i)[j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+
+    public void imprimirMatriz(int[][] matriz) {
+        for (int[] ints : matriz) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(ints[j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
 }
--- src/co/edu/unbosque/model/Tablero.java
+++ src/co/edu/unbosque/model/Tablero.java
@@ -1,181 +1,127 @@
 package co.edu.unbosque.model;
+
 /**
- *Modelamiento del tablero continental, se inicia la matriz con las posiciones dadas, 
- * y algunos atributos como el alto y el ancho del tablero, as� como una varible con valor "2"
- * que nos dir� si una casilla ya est� vac�a.
- * @author Sebastian Moncaleano
+ * Modelamiento del tablero continental, se inicia la matriz con las posiciones dadas,
+ * y algunos atributos como el alto y el ancho del tablero, así como una varible con valor "2"
+ * que nos dirá si una casilla ya está vacía.
  *
+ * @author Sebastian Moncaleano
  */
 public class Tablero {
 
-	private final static int FICHA = 1;
-	private final static int ANCHO = 7;
-	private final static int ALTO = 7;
-	private final static int VACIO = 2;
-	
-	
-	private String[] direcciones = {"ARRIBA","ABAJO","DERECHA","IZQUIERDA"};
-	
-	
-	private int[][] matriz =  {
-			{0, 0, 1, 1, 1, 0, 0},{0, 0, 1, 1, 1, 0, 0}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 2, 1, 1, 1}, 
-			{1, 1, 1, 1, 1, 1, 1}, {0, 0, 1, 1, 1, 0, 0}, {0, 0, 1, 1, 1, 0, 0}};
-	
-	public Tablero() {
-		 
-	}
-	
-
-	
-	public String[] getDirecciones() {
-		return direcciones;
-	}
+    private final static int FICHA = 1;
+    private final static int ANCHO = 7;
+    private final static int ALTO = 7;
+    private final static int VACIO = 2;
 
 
+    private final String[] direcciones = {"ARRIBA", "ABAJO", "DERECHA", "IZQUIERDA"};
 
 
-	public void setDirecciones(String[] direcciones) {
-		this.direcciones = direcciones;
-	}
+    private final int[][] matriz = {
+            {0, 0, 1, 1, 1, 0, 0}, {0, 0, 1, 1, 1, 0, 0}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 2, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1}, {0, 0, 1, 1, 1, 0, 0}, {0, 0, 1, 1, 1, 0, 0}};
 
+    public Tablero() {
 
-
-
-	public int[][] getMatriz() {
-		return matriz;
-	}
-
-
-
-
-	public void setMatriz(int[][] matriz) {
-		this.matriz = matriz;
-	}
-
-
-
-
-	public static int getAncho() {
-		return ANCHO;
-	}
-
-
-
-
-	public static int getAlto() {
-		return ALTO;
-	}
-
-
-
-
-	public void rellenarMatriz(Tablero tablero, int[][] matrizRecibida) {
-		for(int i=0;i<ANCHO;i++) {
-			for(int j=0;j<ALTO;j++) {
-				matrizRecibida[i][j] = tablero.matriz[i][j];
-			}
-		}
-	}
-	
-
-	private boolean sePuedeMover(int i, int j, int nuevaFila, int nuevaColumna) {
-		 return 0 <= i && i < matriz.length 
-                 && 0 <= j && j < matriz[i].length 
-                 && 0 <= nuevaFila && nuevaFila < matriz.length 
-                 && 0 <= nuevaColumna && nuevaColumna < matriz[nuevaFila].length
-                 && matriz[nuevaFila][nuevaColumna] == VACIO
-                 && matriz[(i + nuevaFila) / 2][(j + nuevaColumna) / 2] == FICHA
-                 && matriz[i][j] == FICHA;
-	}
-	
-
-	public boolean move(int i, int j,String direccion) {
-		int nuevaFila = posicionFila(i,direccion);
-		int nuevaColumna = posicionColumna(j,direccion);
-		
-		if(sePuedeMover(i,j,nuevaFila,nuevaColumna)) {
-			colocarFicha(nuevaFila,nuevaColumna);
-			vaciarCelda(i, j);
-			vaciarCelda((i+nuevaFila)/2,(j+nuevaColumna)/2);
-			return true;
-		}
-		return false;
-	}
-	
-	public void retroceder(int i,int j, String direccion) {
-		int nuevaFila = posicionFila(i,direccion);
-		int nuevaColumna = posicionColumna(j,direccion);
-		
-		vaciarCelda(nuevaFila,nuevaColumna);
-		colocarFicha(i,j);
-		colocarFicha((i+nuevaFila)/2,(j+nuevaColumna)/2);
-	}
-	
-
-	private int posicionFila(int i, String direccion) {
-		switch(direccion) {
-		case "DERECHA":
-			return i+2;
-		case "IZQUIERDA":
-			return i-2;
-		}
-		return i;
-	}
-	
-
-	private int posicionColumna(int j, String direccion) {
-		switch(direccion) {
-		case "ARRIBA":
-			return j-2;
-		case "ABAJO":
-			return j+2;
-		}
-		return j;
-	}
-	
-	
-	public boolean celdaLlena(int i, int j) {
-		return matriz[i][j] == FICHA;
-	}
-	
-
-	public boolean isSolucion(int[][] matriz) {
-    	int contador = 0;
-    	for(int i=0;i<matriz.length;i++) {
-    		for(int j=0;j<matriz[0].length;j++) {
-    			if(matriz[i][j] == 1)
-    				contador ++;
-    		}
-    	}
-    	return contador==1&&matriz[3][3]==1?true:false;
     }
-	
-	
-	private void colocarFicha(int i, int j) {
-		matriz[i][j] = FICHA;
-	}
-	
-	private void vaciarCelda(int i, int j) {
-		matriz[i][j] = VACIO;
-	}
-	
-	
-	public void imprimirMatriz() {
-        for (int i = 0; i < matriz.length; i++) {
-                for (int j = 0; j < matriz[i].length; j++) {
-                        System.out.print(matriz[i][j]);
-                }
-                System.out.println();
+
+
+    public String[] getDirecciones() {
+        return direcciones;
+    }
+
+
+    public int[][] getMatriz() {
+        return matriz;
+    }
+
+
+    public static int getAncho() {
+        return ANCHO;
+    }
+
+
+    public static int getAlto() {
+        return ALTO;
+    }
+
+
+    public void rellenarMatriz(Tablero tablero, int[][] matrizRecibida) {
+        for (int i = 0; i < ANCHO; i++) {
+            System.arraycopy(tablero.matriz[i], 0, matrizRecibida[i], 0, ALTO);
         }
-        System.out.println();
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    }
+
+
+    private boolean sePuedeMover(int i, int j, int nuevaFila, int nuevaColumna) {
+        return 0 <= i && i < matriz.length
+                && 0 <= j && j < matriz[i].length
+                && 0 <= nuevaFila && nuevaFila < matriz.length
+                && 0 <= nuevaColumna && nuevaColumna < matriz[nuevaFila].length
+                && matriz[nuevaFila][nuevaColumna] == VACIO
+                && matriz[(i + nuevaFila) / 2][(j + nuevaColumna) / 2] == FICHA
+                && matriz[i][j] == FICHA;
+    }
+
+
+    public boolean move(int i, int j, String direccion) {
+        int nuevaFila = posicionFila(i, direccion);
+        int nuevaColumna = posicionColumna(j, direccion);
+
+        if (sePuedeMover(i, j, nuevaFila, nuevaColumna)) {
+            colocarFicha(nuevaFila, nuevaColumna);
+            vaciarCelda(i, j);
+            vaciarCelda((i + nuevaFila) / 2, (j + nuevaColumna) / 2);
+            return true;
+        }
+        return false;
+    }
+
+    public void retroceder(int i, int j, String direccion) {
+        int nuevaFila = posicionFila(i, direccion);
+        int nuevaColumna = posicionColumna(j, direccion);
+
+        vaciarCelda(nuevaFila, nuevaColumna);
+        colocarFicha(i, j);
+        colocarFicha((i + nuevaFila) / 2, (j + nuevaColumna) / 2);
+    }
+
+
+    private int posicionFila(int i, String direccion) {
+        return switch (direccion) {
+            case "DERECHA" -> i + 2;
+            case "IZQUIERDA" -> i - 2;
+            default -> i;
+        };
+    }
+
+
+    private int posicionColumna(int j, String direccion) {
+        return switch (direccion) {
+            case "ARRIBA" -> j - 2;
+            case "ABAJO" -> j + 2;
+            default -> j;
+        };
+    }
+
+    public boolean isSolucion(int[][] matriz) {
+        int contador = 0;
+        for (int[] ints : matriz) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (ints[j] == 1)
+                    contador++;
+            }
+        }
+        return contador == 1 && matriz[3][3] == 1;
+    }
+
+
+    private void colocarFicha(int i, int j) {
+        matriz[i][j] = FICHA;
+    }
+
+    private void vaciarCelda(int i, int j) {
+        matriz[i][j] = VACIO;
+    }
 }
